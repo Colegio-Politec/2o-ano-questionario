97a57141cbd92bb8b029741ef119a625a00153a4
Gustavo Villalta <396968+gvillalta99@users.noreply.github.com>
2024-04-03 08:48:06 -0300

    Atualizando a proposta da atividade

--- README.md
+++ README.md
@@ -5,3 +5,25 @@ desenvolver essa atividades, fiquem a vontade para revisarem o
 conteúdo no repositório [Colegio-Politec/2o-ano-intro-javascript][6]. Os tópicos
 abordados nesse projeto serão:
 
+- HTML:
+  - Formularios
+  - Inputs
+  - Botões
+- DOM:
+  - Eventos
+  - Criação de elementos
+- JavaScript:
+  - Funções
+  - Objetos
+  - Classes
+
+## Prova 1o Trimestre
+
+Para a prova do 1o trimestre, quero que vocês desenvolvam a seguintes atividade:
+
+> Ao clicar no botão "Adicionar Pergunta", o código JavaScript deve adicionar um elemento `<li>` 
+com o conteúdo da pergunta digitado no campo `<input id="perguntaTextoInput">`.
+> Caso você use Classes, será dado 1 ponto extra.
+
+[6]:https://github.com/Colegio-Politec/2o-ano-intro-javascript-atividades
+
--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
-document.getElementById('adicionarPerguntaForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  const questionInput = document.getElementById('perguntaTextoInput');
-
-  if (questionInput.value.trim() !== '') {
-    // Adicione o código que insere a pergunta como um <li> na
-    // <ul id="questionarioList>"
-
-    questionInput.value = ''; // Limpa o campo após adicionar
-  } else {
-    alert("Por favor, digite uma pergunta.");
-  }
-});
-
+// Para pegar um elemento do HTML com o Javascript, use:
+//  document.getElementById("ID DO ELEMENTO")
+// O evento de interesse para essa atividade é o "submit" do formulário.
+// Os elementos <input>  guardam o valor do texto digitado neles no atributo `.value`.
+// Lembre-se de que não queremos que o formulário seja enviado, para isso você deve usar o `e.preventDefault()`
